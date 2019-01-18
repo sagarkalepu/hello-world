3a04bb77a2859defd1978824af63812d97df3c41
root <sagarkalepu99@gmail.com>
2019-01-18 09:22:45 +0000

    srvr

---
+++ app.js
@@ -0,0 +1,21 @@
+var server = require("express")();
+var http = require("http").createServer(server);
+var socket = require("socket.io")(http);
+
+socket.on('connection', function(io){
+    let rm = "";
+    console.log('a user connected');
+    io.on("setchatroom", (msg) => {
+        rm = msg;
+        io.join(rm);
+    });
+    
+    io.on("message",(msg) => {
+        socket.in(rm).emit("message", msg);
+    })
+});
+
+
+http.listen(4000, () => {
+    console.log("listening on the port 4000");
+})
